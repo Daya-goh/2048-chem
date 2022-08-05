dd00d185c08dec49e6cfa692db8da6800f5804eb
jgqy93 <jgqy93@gmail.com>
2022-08-05 21:59:23 +0800

    generate 2 tiles on start completed

--- app.js
+++ app.js
@@ -7,49 +7,44 @@ import "./main.css";
 const game = {
   page: "#game-page",
 };
+/* ----------- generate random tile ----------- */
+/* ----- pick a random grid cell container ----- */
 
+//creating a tile
+class tile {
+  constructor(atomicNum, elementName) {
+    this.atomicNum = atomicNum;
+    this.elementName = elementName;
+  }
+  newTile = () => {
+    //creating a tile div
+    const randomContainer = Math.floor(Math.random() * 8);
+    console.log(randomContainer);
+
+    const $tile = $("<div>").addClass("tile");
+    $(".grid-cell").eq(randomContainer).append($tile);
+
+    const $atomicNum = $("<div>").addClass("atomicNum").text(this.atomicNum);
+    $tile.append($atomicNum);
+    const $elementName = $("<div>").addClass("element").text(this.elementName);
+    $atomicNum.append($elementName);
+  };
+}
+
+const hydrogen = new tile(1, "H");
+
+const onStart = () => {
+  hydrogen.newTile();
+  hydrogen.newTile();
+};
+onStart();
+//const helium = new tile(2, "He");
 /* ------------------------------- game logic ------------------------------- */
 //update screen
 const render = () => {
   $(".page").hide();
   $(game.page).show();
 };
-
-/* ----------- generate random tile ----------- */
-/* ---------- pick a random grid cell container --------- */
-
-const randomContainer = Math.ceil(Math.random() * 9);
-console.log(randomContainer);
-//creating a tile
-class tile {
-  constructor(atomicNum, element) {
-    this.atomicNum = atomicNum;
-    this.element = element;
-
-    //creating a tile div
-    const $tile = $("<div>").addClass("tile");
-    $(".grid-cell").eq(randomContainer).append($tile);
-
-    const $atomicNum = $("<div>").addClass("atomicNum").text(this.atomicNum);
-    $tile.append($atomicNum);
-    const $element = $("<div>").addClass("element").text(this.element);
-    $atomicNum.append($element);
-  }
-}
-const hydrogen = new tile(1, "H");
-//const helium = new tile(2, "He");
-
-// //creating a tile div
-// const $tile = $("<div>").addClass("tile");
-// $(".cell-1").append($tile);
-
-// const $atomicNum = $("<div>").addClass("atomicNum").text(hydrogen.atomicNum);
-// $tile.append($atomicNum);
-// const $element = $("<div>").addClass("element").text(hydrogen.element);
-// $atomicNum.append($element);
-
-// console.log(hydrogen);
-
 
 /* -------------------------------- game play ------------------------------- */
 
