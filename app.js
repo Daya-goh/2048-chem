752a073494ae18f366a6d19f7891ec1f44c6a544
jgqy93 <jgqy93@gmail.com>
2022-08-05 16:02:51 +0800

    page transition done

--- app.js
+++ app.js
@@ -5,22 +5,40 @@ import "./main.css";
 
 /* ------------------------------ game content ------------------------------ */
 const game = {
-  page: ".start-page",
+  page: "#start-page",
 };
 
 /* ------------------------------- game logic ------------------------------- */
-// const render = () => {
-//   $(".game").hide();
-//   $(".score-page").hide();
-//   $(page.game).show();
-// };
+const render = () => {
+  $(".page").hide();
+  $(game.page).show();
+};
 
 /* ----------------------------- page transition ---------------------------- */
 
-// const pageTransition = () => {
-//   $(".start-button").on("click", () => {
-//     game.page = ".game";
-//     render();
-//   });
-// };
-// pageTransition();
+const pageTransition = () => {
+  $(".start-button").on("click", () => {
+    game.page = "#game-page";
+    render();
+  });
+
+  $(".score-button").on("click", () => {
+    console.log("score-board click");
+    game.page = "#score-page";
+    render();
+  });
+
+  $(".retry-button").on("click", () => {
+    game.page = "#score-page";
+    render();
+  });
+
+  $(".home-button").on("click", () => {
+    console.log("click home");
+    game.page = "#start-page";
+    render();
+  });
+  render();
+};
+
+pageTransition();
