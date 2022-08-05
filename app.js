069d9150b270330eaeae74f0f144db2404e8d7ab
jgqy93 <jgqy93@gmail.com>
2022-08-05 15:09:44 +0800

    html set-up complete

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 import $ from "jquery";
 console.log($);
 import "./main.css";
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------ game content ------------------------------ */
+const game = {
+  page: ".start-page",
+};
+
+/* ------------------------------- game logic ------------------------------- */
+// const render = () => {
+//   $(".game").hide();
+//   $(".score-page").hide();
+//   $(page.game).show();
+// };
+
+/* ----------------------------- page transition ---------------------------- */
+
+// const pageTransition = () => {
+//   $(".start-button").on("click", () => {
+//     game.page = ".game";
+//     render();
+//   });
+// };
+// pageTransition();
