bd83689503beac7d0864ad76dc7d21d8b56bad01
jgqy93 <jgqy93@gmail.com>
2022-08-09 23:10:40 +0800

    4 buttons complete

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ const game = {
   page: "#game-page",
   rows: 4,
   columns: 4,
-  // gameBoard: [
-  //   [0, 0, 0, 0],
-  //   [0, 0, 0, 0],
-  //   [0, 0, 0, 0],
-  //   [0, 0, 0, 0],
-  // ],
   gameBoard: [
-    [2, 2, 2, 2],
-    [4, 4, 8, 4],
-    [4, 4, 2, 2],
-    [0, 0, 16, 16],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
   ],
+  // gameBoard: [
+  //   [2, 4, 8, 16],
+  //   [4, 4, 8, 4],
+  //   [4, 4, 2, 2],
+  //   [0, 0, 16, 16],
+  // ],
 };
 
 /* ------------------------------------------------------ */
@@ -67,12 +67,19 @@ const updateGameBoard = () => {
   }
 };
 
+// https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript -transposing matrix
+const transpose = (gameBoardArray) => {
+  return gameBoardArray[0].map((col, c) =>
+    gameBoardArray.map((row, r) => gameBoardArray[r][c])
+  );
+};
+
 const newTile = () => {
   while (true) {
     let r = Math.round(Math.random() * 3);
     let c = Math.round(Math.random() * 3);
     if (game.gameBoard[r][c] === 0) {
-      game.gameBoard[r][c] = 2;
+      game.gameBoard[r][c] = 1;
       return;
     } else {
       r = Math.round(Math.random() * 3);
@@ -100,8 +107,10 @@ const putZeroBack = (rowArray, arrayMovement) => {
           rowArray.unshift(0);
           break;
         case "moveDown":
-          rowArray.push(0);
+          rowArray.unshift(0);
           break;
+        case "moveUp":
+          rowArray.push(0);
       }
     }
   }
@@ -142,9 +151,58 @@ const moveRight = (rowArray) => {
   return rowArray;
 };
 
-// https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript -transposing matrix
+const moveDown = (rowArray) => {
+  for (let i = 3; i >= 0; i--) {
+    if (rowArray[i] === rowArray[i - 1]) {
+      rowArray[i] = rowArray[i - 1] * 2;
+      rowArray[i - 1] = 0;
+    }
+  }
 
-//button click
+  rowArray = clearZeroTiles(rowArray);
+  putZeroBack(rowArray, "moveDown");
+  return rowArray;
+};
+
+const moveUp = (rowArray) => {
+  rowArray = clearZeroTiles(rowArray);
+
+  for (let i = 0; i < rowArray.length; i++) {
+    if (rowArray[i] === rowArray[i + 1]) {
+      rowArray[i] = rowArray[i + 1] * 2;
+      rowArray[i + 1] = 0;
+    }
+  }
+
+  rowArray = clearZeroTiles(rowArray);
+  putZeroBack(rowArray, "moveUp");
+  return rowArray;
+};
+
+/* -------------------- button click -------------------- */
+$(".up").on("click", () => {
+  console.log("click");
+  const newBoard = transpose(game.gameBoard);
+  for (let r = 0; r < game.rows; r++) {
+    let rowArray = moveUp(newBoard[r]); //move current row and set it back to current row
+    newBoard[r] = rowArray;
+    game.gameBoard = transpose(newBoard); //put it back into the grid
+  }
+  updateGameBoard();
+  newTile();
+});
+
+$(".down").on("click", () => {
+  const newBoard = transpose(game.gameBoard);
+  for (let r = 3; r >= 0; r--) {
+    let rowArray = moveDown(newBoard[r]); //move current row and set it back to current row
+    newBoard[r] = rowArray;
+    game.gameBoard = transpose(newBoard); //put it back into the grid
+  }
+  updateGameBoard();
+  newTile();
+});
+
 $(".left").on("click", () => {
   for (let r = 0; r < game.rows; r++) {
     let rowArray = moveLeft(game.gameBoard[r]); //move current row and set it back to current row
